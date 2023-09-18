9708bd8324cbef2fc761ccba8138be4f3d6f1789
Ashik045 <ashikurislam045@gmail.com>
2023-09-18 15:21:34 +0600

    map component done

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Sidebar from '../Sidebar/Sidebar'
+import VehiclesMap from '../VehiclesMap/VehiclesMap'
 import './home.scss'
 
 const Home = () => {
@@ -10,11 +11,11 @@ const Home = () => {
        
 
         <div className="home_main">
-        <div className="home_sidebar">
-            <Sidebar />
-        </div>
-            <div className="home_items">
-                <h1>vehicle tracking app</h1>
+            <div className="home_sidebar">
+                <Sidebar />
+            </div>
+            <div className="vehicles">
+                <VehiclesMap />
             </div>
         </div>
     </div>
